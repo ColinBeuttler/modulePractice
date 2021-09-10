111e8cae7c3c93cf1a6d82fce6168386c02cd861
Colin Beuttler <beutt102@mail.chapman.edu>
2021-09-10 14:39:14 -0700

    npmi

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ import add from './shoppingCart.js';
 //
 
 console.log('Importing Module');
+
+const shoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
