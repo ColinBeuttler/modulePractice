52aae15c0b4f435a1aa368e6097dc5cf3abf4c24
Colin Beuttler <beutt102@mail.chapman.edu>
2021-09-10 12:57:38 -0700

    moduleprac

---
+++ script.js
@@ -0,0 +1,14 @@
+// importing module
+
+// import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+
+// addToCart('bread', 5);
+
+// import * as shoppingCart from './shoppingCart.js';
+
+import add from './shoppingCart.js';
+
+// shoppingCart.addToCart('bread', 5);
+//
+
+console.log('Importing Module');
